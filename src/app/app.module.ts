81263acf298d929535470e6cfbd7c52771cd779a
japps1a15 <japps1a15@cicct.local>
2017-08-29 10:27:49 +0800

    Students

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { DataentryComponent } from './dataentry/dataentry.component';
+import { InsertdataComponent } from './insertdata/insertdata.component';
+import { PrintdataComponent } from './printdata/printdata.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataentryComponent,
+    InsertdataComponent,
+    PrintdataComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/printdata/printdata.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-printdata',
+  templateUrl: './printdata.component.html',
+  styleUrls: ['./printdata.component.css']
+})
+export class PrintdataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
